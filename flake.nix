850bf5d3cdd307f289e1794f2181ee05216c78c1
niten <niten@fudo.org>
2023-09-04 22:07:33 -0700

    Updated flake.lock

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Nexus Client";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-22.05";
+    nixpkgs.url = "nixpkgs/nixos-23.05";
     utils.url = "github:numtide/flake-utils";
     helpers = {
       url = "git+https://git.fudo.org/fudo-public/nix-helpers.git";
