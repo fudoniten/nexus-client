51237607d25fbf9d37e6b7ae8e518153792a84f6
niten <niten@fudo.org>
2025-06-07 10:38:01 -0700

    feat: Add initial test files for nexus.client and nexus.client.cli

---
+++ test/nexus/client/cli_test.clj
@@ -0,0 +1,3 @@
+(ns nexus.client.cli-test
+  (:require [nexus.client.cli :as sut]
+            [clojure.test :as t]))
---
+++ test/nexus/client_test.clj
@@ -0,0 +1,3 @@
+(ns nexus.client-test
+  (:require [nexus.client :as sut]
+            [clojure.test :as t]))
